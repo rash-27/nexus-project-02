08f2c30fe017a5dbef3701e79db4361ca0e3ba76
rash-27 <rashmik2705@gmail.com>
2024-05-23 22:48:25 +0530

    add auth middleware

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,38 @@
 import {Hono} from 'hono'
-const userRouter = new Hono()
+import { verify } from 'hono/jwt'
+const userRouter = new Hono<{
+    Bindings :{
+        DATABASE_URL : string,
+        JWT_SECRET : string 
+    },
+    Variables : {
+        userId : string
+    }
+}>()
+
+userRouter.use('/*',async(c , next)=>{
+    //token of form "Bearer token"
+    const authHeader = c.req.header('authorization') || '';
+    const token = authHeader?.split(' ')[1];
+    try{
+        const user = await verify(token , c.env.JWT_SECRET);
+
+        if(user){
+            c.set("userId",String(user.id));
+            await next(); 
+        }else {
+            return c.json({
+                message : "You are not logged in!"
+            })
+        }
+    }catch(e){
+        c.status(403)
+        return c.json({
+            message : "You are not authorized" 
+        })
+    }
+
+})
 
 userRouter.get('/me', (c) => {
     return c.text('User Info')
